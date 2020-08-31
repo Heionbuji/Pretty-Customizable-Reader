924b1231e56ab9c17fe9103605c07f584c39a8f1
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-08-31 13:44:25 +0300

    Use QLineEdit instead of QTextEdit in settings

--- src/settingswindow.cpp
+++ src/settingswindow.cpp
@@ -24,7 +24,7 @@ void SettingsWindow::on_okButton_clicked()
 {
     settings.setValue("reader/resetScrollOnPageChange", this->ui->scrollResetCheckbox->isChecked());
     settings.setValue("reader/resetZoomOnPageChange", this->ui->zoomResetCheckbox->isChecked());
-    settings.setValue("mainWindow/bg-color", this->ui->backgroundColor->toPlainText());
+    settings.setValue("mainWindow/bg-color", this->ui->backgroundColor->text());
     settings.setValue("reader/fullscreen", this->ui->fullscreenCheckbox->isChecked());
     settings.sync();
     emit settingsUpdated();
@@ -35,6 +35,6 @@ void SettingsWindow::loadSettings()
 {
     this->ui->scrollResetCheckbox->setChecked(settings.value("reader/resetScrollOnPageChange").toBool());
     this->ui->zoomResetCheckbox->setChecked(settings.value("reader/resetZoomOnPageChange").toBool());
-    this->ui->backgroundColor->setPlainText(settings.value("mainWindow/bg-color").toString());
+    this->ui->backgroundColor->setText(settings.value("mainWindow/bg-color").toString());
     this->ui->fullscreenCheckbox->setChecked(settings.value("reader/fullscreen").toBool());
 }
